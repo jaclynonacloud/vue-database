3a159beffb99f257fad4f31f55f25132c425edaa
Jaclyn Staples <jaclynonacloud@gmail.com>
2019-03-12 15:22:10 -0300

    Switched mongoose order.

--- src/server.js
+++ src/server.js
@@ -10,15 +10,15 @@ const config = require('./db/config.js')
 
 require('dotenv').load()
 
+app.use(cors())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     () => {console.log('Database is connected') },
     err => { console.log('Can not connect to the database'+ err)}
 );
-
-app.use(cors())
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 app.use(express.static('./dist'))
 
